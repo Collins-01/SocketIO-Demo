8cc11145e4a26137c1d1d3dded868e65d0e4b50c
Collins Oriakhi <collins.oriakhi@itara.co>
2022-05-30 10:25:52 +0100

    Added Nodemon to restart server

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-import express from "express";
-import { Socket } from "socket.io";
-const PORT = 3000;
+const express = require('express');
+var http = require('http')
+var io = require('socket.io')
 
+const  cors = require('cors')
 const app= express()
 
-app.listen((v)=>{
-    console.log('Server  has started')
+const PORT = process.env.PORT ||  3000;
+
+var server = http.createServer(app);
+app.use(express.json())
+// app.use(cors())
+const socketIO = io.Server
+
+
+// socketIO.on("Connection", (socket)=>{
+//     console.log("Socket COnnection")
+// })
+server.listen(PORT, ()=> {
+    console.log(`Server Started on PORT : ${PORT}`)
 })
+
