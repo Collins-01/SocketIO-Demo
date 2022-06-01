50e2e92f4e5d96d4043296412a1e2e3d684fdd8e
Collins Oriakhi <collins.oriakhi@itara.co>
2022-06-01 22:00:58 +0100

    A user can connect to local server

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
+const express = require('express')
+const app= express();
+const http= require('http')
+const server= http.createServer(app)
+const {Server} = require('socket.io')
+const io = new Server(server)
 
 
-const server = require('http').createServer()
-const io = require('socket.io')(server)
 
-io.on('connection', function (client) {
 
-  console.log('client connect...', client.id);
-
-  client.on('typing', function name(data) {
-    console.log(data);
-    io.emit('typing', data)
-  })
-
-  client.on('message', function name(data) {
-    console.log(data);
-    io.emit('message', data)
-  })
-
-  client.on('location', function name(data) {
-    console.log(data);
-    io.emit('location', data);
-  })
-
-  client.on('connect', function () {
-  })
-
-  client.on('disconnect', function () {
-    console.log('client disconnect...', client.id)
-    // handleDisconnect()
-  })
-
-  client.on('error', function (err) {
-    console.log('received error from client:', client.id)
-    console.log(err)
-  })
+io.on('connection', (socket)=>{
+  console.log("A user connected")
 })
-
-var server_port = process.env.PORT || 3000;
-server.listen(server_port, function (err) {
-  if (err) throw err
-  console.log('Listening on port %d', server_port);
-});
+const PORT= 3000;
+server.listen(PORT, ()=>{
+  console.log(`Server has started on PORT : ${PORT}`)
+})
