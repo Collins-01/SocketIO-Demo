18403dbe535f4db4a99864fa740c31464aa247f8
Collins Oriakhi <collins.oriakhi@itara.co>
2022-06-01 23:35:20 +0100

    User can connect and send messages

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ const server= http.createServer(app)
 const {Server} = require('socket.io')
 const io = new Server(server)
 
+const messages =[];
 
 
 
 io.on('connection', (socket)=>{
   console.log("A user connected")
+  const username= socket.handshake.query.username
+  console.log(`Connected User -> ${username}`)
+  socket.on('message', (data)=>{
+    const message = {
+      message: data.message,
+      sender: username,
+      sentAt: Date.now()
+    }
+    console.log(message);
+    messages.push(message)
+    io.emit('message', message)
+  })
 })
+
+
+
 const PORT= 3000;
 server.listen(PORT, ()=>{
   console.log(`Server has started on PORT : ${PORT}`)
-})
+}) 
