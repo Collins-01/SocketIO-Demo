5efb86faea1c73f6a40ce3d2530eff4c54f5144f
Collins Oriakhi <collins.oriakhi@itara.co>
2022-06-03 10:55:13 +0100

    Added some documented algorithm for READ, SENT, and Delivered messages

--- index.js
+++ index.js
@@ -12,22 +12,43 @@ const messages =[];
 io.on('connection', (socket)=>{
   console.log("A user connected")
   const username= socket.handshake.query.username
+  const id= socket.handshake.query.id
   console.log(`Connected User -> ${username}`)
   socket.on('message', (data)=>{
+   
     const message = {
-      message: data.message,
-      sender: username,
-      sentAt: Date.now()
+      id : data.id,
+      msg: data.msg,
+      recieverID: data.recieverID,
+      senderID: data.senderID,
+      sentAt: data.sentAt
     }
     console.log(message);
     messages.push(message)
     io.emit('message', message)
   })
+
+  
+  
 })
 
-
+io.on('typing',(data)=>{
+  console.log(data)
+  io.emit('typing', `${username} is Typing...`)
+})
 
 const PORT= 3000;
 server.listen(PORT, ()=>{
   console.log(`Server has started on PORT : ${PORT}`)
 }) 
+
+
+/*
+emits sent()=> when a user sends a message, and  there is network connection to send the message to the server.
+emits delivered(messageID)=> when the message gets to the sever, it emits a delivered response to the user, and updates the message in the SQL table[false to true]
+emits read(messageID)=> when the message gets delivered to Alice, it gets saved in the SQL table. when Alice opens a chat with a particular user, it emits [Read] to all the messages that show delivered to that user.
+it should take a list of messageIDs
+example, in the table messages..... SELECT * FROM messages WHERE
+
+
+*/
